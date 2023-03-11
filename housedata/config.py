abdd2f039fae6ced7c39493b02f5824751802537
Tim Huang <t8522192@gmail.com>
2023-03-11 16:39:06 +0800

    feat: send message to telegram user

---
+++ housedata/config.py
@@ -0,0 +1,6 @@
+import os
+
+TIM_TELEGRAM_ID = os.environ.get("TIM_TELEGRAM_ID", "")
+GROUP_TELEGRAM_ID = os.environ.get("GROUP_TELEGRAM_ID", "")
+CA_TELEGRAM_ID = os.environ.get("CA_TELEGRAM_ID", "")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
