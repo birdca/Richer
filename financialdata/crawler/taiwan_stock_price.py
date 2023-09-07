1e495ce960ab2b09659ff8bcbda70213aae1cf7c
Tim Huang <t8522192@gmail.com>
2023-09-07 08:39:45 +0800

    test: run unit test on pr

--- financialdata/crawler/taiwan_stock_price.py
+++ financialdata/crawler/taiwan_stock_price.py
@@ -1,3 +1,9 @@
+"""
+由於書本排版, 
+這裡使用 black -l 40 taiwan_stock_price.py
+調整程式最大行數,
+使用者可再自行調整
+"""
 import datetime
 import time
 import typing
@@ -9,8 +15,10 @@ from financialdata.schema.dataset import (
     check_schema,
 )
 
+
 def is_weekend(day: int) -> bool:
     return day in [5, 6]
+
 
 def gen_task_paramter_list(
     start_date: str, end_date: str
@@ -48,6 +56,7 @@ def gen_task_paramter_list(
     ]
     return date_list
 
+
 def clear_data(
     df: pd.DataFrame,
 ) -> pd.DataFrame:
@@ -79,57 +88,6 @@ def clear_data(
     return df
 
 
-def set_column(
-    df: pd.DataFrame,
-) -> pd.DataFrame:
-    """設定資料欄位名稱"""
-    df.columns = [
-        "StockID",
-        "Close",
-        "Change",
-        "Open",
-        "Max",
-        "Min",
-        "TradeVolume",
-        "TradeValue",
-        "Transaction",
-    ]
-    return df
-
-
-def tpex_header():
-    """網頁瀏覽時, 所帶的 request header 參數, 模仿瀏覽器發送 request"""
-    return {
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Connection": "keep-alive",
-        "Host": "www.tpex.org.tw",
-        "Referer": "https://www.tpex.org.tw/web/stock/aftertrading/otc_quotes_no1430/stk_wn1430.php?l=zh-tw",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
-    }
-
-def twse_header():
-    """網頁瀏覽時, 所帶的 request header 參數, 模仿瀏覽器發送 request"""
-    return {
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Connection": "keep-alive",
-        "Host": "www.twse.com.tw",
-        "Referer": "https://www.twse.com.tw/zh/page/trading/exchange/MI_INDEX.html",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
-    }
-
-
-def convert_date(date: str) -> str:
-    year, month, day = date.split("-")
-    year = int(year) - 1911
-    return f"{year}/{month}/{day}"
-
-
 def colname_zh2en(
     df: pd.DataFrame,
     colname: typing.List[str],
@@ -158,6 +116,52 @@ def colname_zh2en(
         for col in colname
     ]
     df = df.drop([""], axis=1)
+    return df
+
+
+def twse_header():
+    """網頁瀏覽時, 所帶的 request header 參數, 模仿瀏覽器發送 request"""
+    return {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Connection": "keep-alive",
+        "Host": "www.twse.com.tw",
+        "Referer": "https://www.twse.com.tw/zh/page/trading/exchange/MI_INDEX.html",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+
+
+def tpex_header():
+    """網頁瀏覽時, 所帶的 request header 參數, 模仿瀏覽器發送 request"""
+    return {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Connection": "keep-alive",
+        "Host": "www.tpex.org.tw",
+        "Referer": "https://www.tpex.org.tw/web/stock/aftertrading/otc_quotes_no1430/stk_wn1430.php?l=zh-tw",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+
+
+def set_column(
+    df: pd.DataFrame,
+) -> pd.DataFrame:
+    """設定資料欄位名稱"""
+    df.columns = [
+        "StockID",
+        "Close",
+        "Change",
+        "Open",
+        "Max",
+        "Min",
+        "TradeVolume",
+        "TradeValue",
+        "Transaction",
+    ]
     return df
 
 
@@ -201,12 +205,17 @@ def crawler_twse(
     https://www.twse.com.tw/zh/page/trading/exchange/MI_INDEX.html
     """
     logger.info("crawler_twse")
+    # headers 中的 Request url
+    url = "https://www.twse.com.tw/exchangeReport/MI_INDEX?response=json&date={date}&type=ALL"
+    url = url.format(
+        date=date.replace("-", "")
+    )
     # 避免被證交所 ban ip, 在每次爬蟲時, 先 sleep 5 秒
     time.sleep(5)
     # request method
-    url = "https://www.twse.com.tw/exchangeReport/MI_INDEX"
-    payload = {'response': 'json', 'date': date, 'type': 'ALL'}
-    res = requests.post(url, data=payload, headers=twse_header())
+    res = requests.get(
+        url, headers=twse_header()
+    )
     # 2009 年以後的資料, 股價在 response 中的 data9
     # 2009 年以後的資料, 股價在 response 中的 data8
     # 不同格式, 在證交所的資料中, 是很常見的,
@@ -271,6 +280,13 @@ def convert_change(
     df = df.fillna("")
     df = df.drop(["Dir"], axis=1)
     return df
+
+
+def convert_date(date: str) -> str:
+    logger.info("convert_date")
+    year, month, day = date.split("-")
+    year = int(year) - 1911
+    return f"{year}/{month}/{day}"
 
 
 def crawler(
