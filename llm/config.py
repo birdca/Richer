f2dc2bd51d18d0cfc7dd7a1fc5ffec1f78f348c1
Tim Huang <t8522192@gmail.com>
2023-11-24 09:22:33 +0800

    enable openai and langchain

---
+++ llm/config.py
@@ -0,0 +1,3 @@
+import os
+
+OPENAPI_SERVER_HOST = os.environ.get("OPENAPI_SERVER_HOST", "")
---
+++ llm/prompt/message.py
@@ -0,0 +1,12 @@
+import openai
+from langchain.prompts import ChatPromptTemplate
+
+
+def get_completion(prompt, model=llm_model):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message["content"]
