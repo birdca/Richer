ecc99e2d58f426d4c26122d44229080957a65a63
birdca <kiancaca@gmail.com>
2024-03-13 15:58:19 +0800

    fix: add missing file and update Pipfile

--- llm/sample/get_content.py
+++ llm/sample/get_content.py
@@ -2,6 +2,9 @@ import os
 import openai
 from dotenv import load_dotenv, find_dotenv
 
+from langchain_community.document_loaders.blob_loaders.youtube_audio import (
+    YoutubeAudioLoader,
+)
 from langchain_community.document_loaders.generic import GenericLoader
 from langchain_community.document_loaders.parsers.audio import (
     OpenAIWhisperParser,
@@ -11,16 +14,14 @@ from langchain_community.document_loaders import UnstructuredXMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
 
 
-_ = load_dotenv(find_dotenv())
-
-openai.api_key  = os.environ['OPENAI_API_KEY']
+load_dotenv(find_dotenv())
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 
 # transcript data
 local = False # TODO: failed in using local parsing
 yt_urls = ['https://www.youtube.com/watch?v=23OTY_Yy1_A']
-save_dir = 'docs/youtube'
-"""
+save_dir = 'sample/files/youtube'
 if local:
     loader = GenericLoader(
         YoutubeAudioLoader(yt_urls, save_dir), OpenAIWhisperParserLocal()
@@ -28,11 +29,11 @@ if local:
 else:
     loader = GenericLoader(YoutubeAudioLoader(yt_urls, save_dir), OpenAIWhisperParser())
 docs = loader.load()
-"""
+print(docs[:500])
 
 
 # rss data
-rss_url='docs/xml/954689a5-3096-43a4-a80b-7810b219cef3.xml'
+rss_url='sample/files/xml/954689a5-3096-43a4-a80b-7810b219cef3.xml'
 loader = UnstructuredXMLLoader(rss_url)
 docs = loader.load()
-# print(docs[0])
+print(docs[0])
