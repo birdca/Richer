2f5ec26352821377ba91457d78656399bc61e830
birdca <kiancaca@gmail.com>
2024-03-25 15:19:10 +0800

    feat: split xml rss by episodes

--- llm/sample/get_content.py
+++ llm/sample/get_content.py
@@ -11,7 +11,7 @@ from langchain_community.document_loaders.parsers.audio import (
     OpenAIWhisperParserLocal,
 )
 from langchain_community.document_loaders import UnstructuredXMLLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 load_dotenv(find_dotenv())
@@ -19,6 +19,7 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 
 # transcript data
+"""
 local = False # TODO: failed in using local parsing
 yt_urls = ['https://www.youtube.com/watch?v=23OTY_Yy1_A']
 save_dir = 'sample/files/youtube'
@@ -30,10 +31,23 @@ else:
     loader = GenericLoader(YoutubeAudioLoader(yt_urls, save_dir), OpenAIWhisperParser())
 docs = loader.load()
 print(docs[:500])
+"""
 
 
 # rss data
 rss_url='sample/files/xml/954689a5-3096-43a4-a80b-7810b219cef3.xml'
 loader = UnstructuredXMLLoader(rss_url)
-docs = loader.load()
-print(docs[0])
+xml_docs = loader.load()
+# print(xml_docs[0])
+
+# chunck
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2600,
+    chunk_overlap=4,
+    separators=["\n\nEP", "\n\n", "\n", "(?<=\. )", " ", ""]
+)
+xml_splitter = splitter.split_documents(xml_docs)
+for i in range(1, 4):
+    print(xml_splitter[i])
+    print(xml_splitter[i].metadata)
+    print()
